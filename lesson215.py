f4d213f3bb9e0b16ad8192688f4251bf05132194
Mark Peters <mapeters101@earthlink.net>
2024-11-19 13:43:33 -0500

    Lesson215:  Complete work on lesson from 5:50.

--- lesson215.py
+++ lesson215.py
@@ -25,10 +25,37 @@ print(years)
 years.insert(-1, '2025')
 print(years)
 
+print('='*80)
+
+years.clear()
+print(years)
+print()
+l2 = [10, 20, 30, 40]
+print(l2)
+x = l2.pop()
+print(x)
+print(l2)
+print()
+
+y = l2.pop(1)
+#l2.pop(100)
+print(y)
+print(l2)
+print()
+
+l3 = [10, 20, 10, 40, 20]
+l3.remove(10)
+print(l3)
 print()
 
 
-
+l3 = [10, 20, 10, 40, 20, 10, 20, 20, 'z']
+l3.remove(10)
+print(l3)
+while 20 in l3:
+    l3.remove(20)
+print(l3)
+print()
 
 
 
