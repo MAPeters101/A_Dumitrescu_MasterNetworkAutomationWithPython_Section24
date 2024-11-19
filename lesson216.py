4f39ce8f19ec8ac9d28bdd88c52e4393dc1143cd
Mark Peters <mapeters101@earthlink.net>
2024-11-19 16:18:32 -0500

    Lesson216:  Complete work on lesson from 4:10.

--- lesson216.py
+++ lesson216.py
@@ -21,4 +21,48 @@ print()
 print('f' in letters)
 print()
 
+l1 = [1, 3, 'abc', 10, 'x']
+print(f'{l1=}')
+l1.reverse()
+print(f'{l1=}')
+print()
+
+ages = [10, 8, 23, 40, 35]
+print(f'{ages=}')
+la = sorted(ages)
+print(f'{la=}')
+print(f'{ages=}')
+
+ages = [10, 8, 23, 40, 35]
+n = ages.sort()
+print(f'{n=}')
+print(f'{ages=}')
+print()
+
+
+ages = [10, 8, 23, 40, 35]
+print(f'{ages=}')
+la = sorted(ages, reverse=True)
+print(f'{la=}')
+print(f'{ages=}')
+
+ages = [10, 8, 23, 40, 35]
+n = ages.sort(reverse=True)
+print(f'{n=}')
+print(f'{ages=}')
+print()
+
+print('='*80)
+
+l1 = [1, 3, '4']
+#l1.sort()
+print(l1)
+print()
+
+l2 = [9, 10, 5, 100, 46]
+print(f'{max(l2)=}')
+print(f'{min(l2)=}')
+print(f'{sum(l2)=}')
+
+
 
