9163435161c51c25345ce983dec7b415296128e1
Mark Peters <mapeters101@earthlink.net>
2024-11-19 16:28:03 -0500

    Lesson217:  Work on lesson.

---
+++ lesson217.py
@@ -0,0 +1,23 @@
+
+s1 = 'I am learning Python Programming!'
+l1 = s1.split()
+print(l1)
+
+s1 = 'I am lea:rning Pyth:on Prog:ramm:ing!'
+l1 = s1.split(':')
+print(l1)
+print()
+
+ip_list = ['192.168.0.1', '192.168.0.2', '192.168.0.3']
+ip_str = ' '.join(ip_list)
+print(ip_str)
+
+ip_str = ', '.join(ip_list)
+print(ip_str)
+
+ip_str = 'xxx'.join(ip_list)
+print(ip_str)
+
+
+
+
