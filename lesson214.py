341d81dbd84bf4dd98d428a7221539adfc7dc653
Mark Peters <mapeters101@earthlink.net>
2024-11-19 12:50:37 -0500

    Lesson214:  Complete work on lesson from 3:50.

--- lesson214.py
+++ lesson214.py
@@ -22,8 +22,24 @@ l3.append('abc')
 print(f'{hex(id(l1))=} {l1=}')
 print(f'{hex(id(l3))=} {l3=}')
 print()
+print('='*80)
+
+nums = [1, 2, 3, 4, 5, 6, 7, 0, 1, 2]
+
+# for n in nums:
+#     if n < 5:
+#         nums.remove(n)
+# print(nums)
+
+new_list = list()
+for n in nums:
+    if n >= 5:
+        new_list.append(n)
+print(new_list)
 
 
+my_list = [n for n in nums if n >= 5]
+print(my_list)
 
 
 
