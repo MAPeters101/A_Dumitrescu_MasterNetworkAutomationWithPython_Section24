1ef178e11d32654bb99190836609440c010b9319
Mark Peters <mapeters101@earthlink.net>
2024-11-21 17:32:24 -0500

    Lesson222:  Work on lesson.

---
+++ lesson222.py
@@ -0,0 +1,25 @@
+
+
+# 1. Tuples are faster and more efficient than lists.
+
+# 2. Tuples are safer than lists.
+
+# 3. Tuples can be used as keys in dictionaries
+
+# 4. Storage efficiency
+import sys
+l1 = [1, 2, 3, 4, 5, 6]
+t1 = (1, 2, 3, 4, 5, 6)
+print(f'List memory size: {sys.getsizeof(l1)}')
+print(f'Tuple memory size: {sys.getsizeof(t1)}')
+
+
+
+
+
+
+
+
+
+
+
